4e2f117ca21ede61e02b477c2803f4a0991adba7
kevinmillercheme <kevinmiller1242@gmail.com>
2026-03-18 21:33:06 -0400

    all register and authenticate functionality included and complete

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,18 +1,46 @@
 import { createContext, useContext, useState } from "react";
 
-const API = "https://fsa-jwt-practice.herokuapp.com";
-
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
 
-  // TODO: signup
+  const signup = async (formData) => {
+    const username = formData.get("name")
+    const request = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', 
+              { 
+                method: "POST", 
+                headers: { 
+                  "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify({ 
+                  username: username, 
+                  // password: "super-secret-999" 
+                }) 
+              }) 
 
-  // TODO: authenticate
+    const response = await request.json()
+    setToken(response.token)
+    setLocation("TABLET")
 
-  const value = { location };
+  }
+
+  const authenticate = async () => {
+    const request = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
+              { 
+                method: "GET", 
+                headers: { 
+                  "Content-Type": "application/json",
+                  "Authorization": `Bearer ${token}` 
+                }
+              })
+
+    await request.json()
+    setLocation("TUNNEL")
+  }
+
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,5 +1,8 @@
+import { useAuth } from "./AuthContext"
 /** Users can enter their name to receive a token from the API. */
+
 export default function Entrance() {
+  const { signup } = useAuth()
   // TODO: call signup when form is submitted
 
   return (
@@ -7,15 +10,15 @@ export default function Entrance() {
       <h1>Cave Entrance</h1>
       <p>Your journey has brought you to the base of a rocky mountain.</p>
       <p>
-        The quickest path forward is through the mountain's winding tunnels, but
+        The quickest path forward is through the mountains winding tunnels, but
         a sturdy metal gate sits closed before you.
       </p>
       <p>
         Two giant badgers stand guard on either side of the gate, their eyes
         fixed on you. The one on the left opens its mouth, and with a deep,
-        rumbling voice, it asks, "Who approaches? Speak your name."
+        rumbling voice, it asks, "Who approaches? Speak your name"
       </p>
-      <form>
+      <form action = {signup}>
         <label>
           Name
           <input name="name" />
